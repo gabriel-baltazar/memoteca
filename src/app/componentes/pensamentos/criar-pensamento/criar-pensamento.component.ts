9b23d6a93b6c022cbd6995e47f29452a71616bf3
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2023-10-07 07:35:19 -0300

    Finished project in class Angular14: Aplique conceitos

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.scss']
+})
+export class CriarPensamentoComponent implements OnInit {
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
+  }
+
+  constructor(private service: PensamentoService, private router: Router){}
+
+  ngOnInit():void {
+
+  }
+
+  criarPensamento(){
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['listarPensamento'])
+    })
+  }
+  cancelar(){
+    this.router.navigate(['listarPensamento'])
+  }
+}
---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.scss']
+})
+export class PensamentoComponent {
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I love Angular',
+    autoria: 'Nay',
+    modelo: 'modelo3'
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g'
+    }else{
+      return 'pensamento-p'
+    }
+  }
+}

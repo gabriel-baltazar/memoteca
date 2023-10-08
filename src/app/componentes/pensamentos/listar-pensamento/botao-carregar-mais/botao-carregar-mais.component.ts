0db4831f06b191fe26813e23cd6fe1a23d952bb3
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2023-10-08 18:02:02 -0300

    Add pagination

---
+++ src/app/componentes/pensamentos/listar-pensamento/botao-carregar-mais/botao-carregar-mais.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-botao-carregar-mais',
+  templateUrl: './botao-carregar-mais.component.html',
+  styleUrls: ['./botao-carregar-mais.component.scss']
+})
+export class BotaoCarregarMaisComponent {
+  @Input() haMaisPensamentos: boolean = false;
+
+
+
+}
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
 
@@ -11,8 +11,12 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API)
+  listar(pagina: number): Observable<Pensamento[]> {
+    const itensPorPagina = 6
+
+    let params = new HttpParams().set("_page",pagina).set("_limit",itensPorPagina)
+
+    return this.http.get<Pensamento[]>(this.API,{params})
   }
 
   criar(pensamento: Pensamento):Observable<Pensamento>{
@@ -22,13 +26,13 @@ export class PensamentoService {
   editar(pensamento: Pensamento):Observable<Pensamento>{
     const url = `${this.API}/${pensamento.id}`
     return this.http.put<Pensamento>(url, pensamento)
-  } 
-  
+  }
+
   excluir(id: number): Observable<Pensamento> {
     const url = `${this.API}/${id}`
     return this.http.delete<Pensamento>(url)
   }
-  
+
   buscarPorId(id: number): Observable<Pensamento> {
     const url = `${this.API}/${id}`
     return this.http.get<Pensamento>(url)
